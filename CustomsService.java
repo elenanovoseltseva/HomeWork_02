06e24c1c43f75036035c6a49df6e6ae8387bd573
elenanovoseltseva <75611896+elenanovoseltseva@users.noreply.github.com>
2025-11-20 16:19:16 +0400

    Add files via upload

---
+++ CustomsService.java
@@ -0,0 +1,7 @@
+package ru.netology.service;
+
+public class CustomsService {
+
+    public static final int Fees_On_Weight = 100;
+
+}
---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import ru.netology.service.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите цену товара (в руб.):");
+        int price = scanner.nextInt();
+
+        System.out.print("Введите вес товара (в кг.):");
+        int weight = scanner.nextInt();
+
+        int total = (int) calFees(price, weight);
+        System.out.println("Размер пошлины (в руб.) составит: " + total);
+    }
+
+    public static double calFees(int price, int weight) {
+        double total  = price * 0.01 + weight * CustomsService.Fees_On_Weight;
+        return total;
+    }
+}
+
