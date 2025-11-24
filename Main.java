daa1015d7b3ccdff6f1d321708dfbd0755a0d8e8
elenanovoseltseva <75611896+elenanovoseltseva@users.noreply.github.com>
2025-11-24 15:38:32 +0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,17 +6,17 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите цену товара (в руб.):");
+        System.out.print("   Здравствуйте! \nВведите цену товара (в руб.):");
         int price = scanner.nextInt();
 
         System.out.print("Введите вес товара (в кг.):");
         int weight = scanner.nextInt();
 
-        int total = (int) calFees(price, weight);
+        int total = (int) calculateCustoms(price, weight);
         System.out.println("Размер пошлины (в руб.) составит: " + total);
     }
 
-    public static double calFees(int price, int weight) {
+    public static double calculateCustoms(int price, int weight) {
         double total  = price * 0.01 + weight * CustomsService.Fees_On_Weight;
         return total;
     }
