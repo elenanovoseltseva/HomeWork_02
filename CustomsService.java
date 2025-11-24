644f71fd0bbe95c6089bd29b427f8d4b1a1e4926
elenanovoseltseva <75611896+elenanovoseltseva@users.noreply.github.com>
2025-11-24 18:02:14 +0400

    Add files via upload

--- CustomsService.java
+++ CustomsService.java
@@ -2,7 +2,7 @@ package ru.netology.service;
 
 public class CustomsService {
 
-    public static final int Fees_On_Weight = 100;
+    static final int Fees_On_Weight = 100;
 
     public static double calculateCustoms(int price, int weight) {
         double ttl  = price * 0.01 + weight * Fees_On_Weight;
